8f69b820731db47373c0591b9c5415a61bf097ea
Edward Ionides <ionides@umich.edu>
2018-09-14 17:48:49 -0400

    starting notes4

--- 04/notes04.R
+++ 04/notes04.R
@@ -1,19 +1,20 @@
 ## ----setup,echo=F,results=F,cache=F--------------------------------------
 library(broman) # used for myround 
 
-## ----dice_sample,echo=T,eval=F-------------------------------------------
-## ## Make 20 draws with replacement from {1,2,3,4,5,6}
-## ## This models 20 realizations of rolling a fair die
-## ## We can plot a histogram of the simulated dice rolls
-## my_data <- sample(1:6,size=20,replace=TRUE)
-## hist(my_data)
+## ----hist100,echo=T------------------------------------------------------
+hist(replicate(sample(1:6,size=1),n=100),
+  main="", prob=TRUE, breaks=0.5:6.5, xlab="n=100")
 
-## ----fig.width=4,fig.height=2.5,out.width="2.9in",echo=F,eval=T----------
-set.seed(23) 
-par(mai=c(0.1,0.8,0.5,0.5))
-## Make 20 draws with replacement from {1,2,3,4,5,6}
-## This models 20 realizations of rolling a fair die
-## We can plot a histogram of the simulated dice rolls
-my_data <- sample(1:6,size=20,replace=TRUE)
-hist(my_data)
+## ------------------------------------------------------------------------
+plot(1:10, 
+  main="", prob=TRUE, 
+  breaks=0.5:6.5, xlab="n=100")
+
+## ----hist100eval,out.width="60mm",fig.width=4,fig.height=3,echo=F--------
+hist(replicate(sample(1:6,size=1),n=100),
+  main="", prob=TRUE, breaks=0.5:6.5, xlab="n=100")
+
+## ----hist10000eval,out.width="60mm",fig.width=4,fig.height=3,echo=F------
+hist(replicate(sample(1:6,size=1),n=10000),
+  main="", prob=TRUE, breaks=0.5:6.5, xlab="n=10000")
 
