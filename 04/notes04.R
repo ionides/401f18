5771a72c05966e6b79fd91ac0269cb9aae09069f
Edward L Ionides <ionides@umich.edu>
2018-09-15 00:42:42 -0400

    debugging notes04 - hard to find a missing fragile

--- 04/notes04.R
+++ 04/notes04.R
@@ -1,20 +1,11 @@
 ## ----setup,echo=F,results=F,cache=F--------------------------------------
 library(broman) # used for myround 
 
-## ----hist100,echo=T------------------------------------------------------
-hist(replicate(sample(1:6,size=1),n=100),
-  main="", prob=TRUE, breaks=0.5:6.5, xlab="n=100")
+## ----hist100,echo=T,eval=F,purl=TRUE-------------------------------------
+## hist(replicate(n=100,sample(1:6,size=1) ),
+##   main="",prob=TRUE,breaks=0.5:6.5,xlab="n=100",ylim=c(0,0.21))
 
-## ------------------------------------------------------------------------
-plot(1:10, 
-  main="", prob=TRUE, 
-  breaks=0.5:6.5, xlab="n=100")
-
-## ----hist100eval,out.width="60mm",fig.width=4,fig.height=3,echo=F--------
-hist(replicate(sample(1:6,size=1),n=100),
-  main="", prob=TRUE, breaks=0.5:6.5, xlab="n=100")
-
-## ----hist10000eval,out.width="60mm",fig.width=4,fig.height=3,echo=F------
-hist(replicate(sample(1:6,size=1),n=10000),
-  main="", prob=TRUE, breaks=0.5:6.5, xlab="n=10000")
+## ----hist10000,eval=F,echo=F,purl=TRUE-----------------------------------
+## hist(replicate(sample(1:6,size=1),n=10000),
+##   main="", prob=TRUE, breaks=0.5:6.5, xlab="n=10000",ylim=c(0,0.21))
 
