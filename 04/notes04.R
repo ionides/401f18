fd0df6846d9835f2b4989549a29dc5bb164e5de2
Edward L Ionides <ionides@umich.edu>
2018-09-16 10:45:27 -0400

    notes4 ctd

--- 04/notes04.R
+++ 04/notes04.R
@@ -20,3 +20,24 @@ rnorm(n=10,mean=20,sd=5)
 ## hist(rnorm(n=10000,mean=20,sd=5),
 ##   main="",xlab="n=10000")
 
+## ----normal_pdf,out.width="60mm",fig.width=3.5,fig.height=3,echo=F,purl=T----
+# Create data for the area to shade
+pnorm_plot <- function(q,mean,sd,...) {
+  coord.x <- seq(from=mean-3*sd,to=mean+3*sd,by=sd/100)
+  plot(coord.x,dnorm(coord.x,mean,sd),type="l",...)
+  shaded.coord.x <- coord.x[coord.x <= q]
+  polygon.x <- c(mean-3*sd,shaded.coord.x,q)
+  polygon.y <- c(0,dnorm(shaded.coord.x,mean,sd),0)  
+  polygon(polygon.x,polygon.y,col='skyblue')
+}
+pnorm_plot(25,20,5,main="",xlab="",ylab="probability density")
+
+## ----runif,echo=T,eval=F,purl=TRUE---------------------------------------
+## hist(runif(100))
+
+## ----rexp,eval=F,echo=T,purl=TRUE----------------------------------------
+## hist(rexp(100))
+
+## ----eval=F--------------------------------------------------------------
+## NA
+
