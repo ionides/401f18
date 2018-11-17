40e1bef9eabc360aa212437fa26088378be1317d
Ionides <ionides@umich.edu>
2018-11-17 10:42:13 -0500

    some edits to notes08

--- 08/notes08.R
+++ 08/notes08.R
@@ -2,16 +2,23 @@
 # library(broman) # used for myround 
 par(mai=c(0.8,0.8,0.1,0.1))
 
-## ----reconstruct_variables,echo=F----------------------------------------
-L <- read.table(file="life_expectancy.txt",header=TRUE)
-L_fit <- lm(Total~Year,data=L)
-L_detrended <- L_fit$residuals
+## ----reconstruct_variables,echo=F,eval=F---------------------------------
+## U <- read.table(file="unemployment.csv",sep=",",header=TRUE)
+## U_annual <- apply(U[,2:13],1,mean)
+## U_detrended <- lm(U_annual~U$Year)$residuals
+## L <- read.table(file="life_expectancy.txt",header=TRUE)
+## L <- subset(L,L$Year %in% U$Year)
+## L_fit <- lm(Total~Year,data=L)
+## L_detrended <- L_fit$residuals
+
+## ----lm1-----------------------------------------------------------------
 U <- read.table(file="unemployment.csv",sep=",",header=TRUE)
 U_annual <- apply(U[,2:13],1,mean)
 U_detrended <- lm(U_annual~U$Year)$residuals
-L_detrended <- subset(L_detrended,L$Year %in% U$Year)
-
-## ----lm1-----------------------------------------------------------------
+L <- read.table(file="life_expectancy.txt",header=TRUE)
+L <- subset(L,L$Year %in% U$Year)
+L_fit <- lm(Total~Year,data=L)
+L_detrended <- L_fit$residuals
 lm1 <- lm(L_detrended~U_detrended)
 
 ## ----lag_lm--------------------------------------------------------------
@@ -33,43 +40,4 @@ plot(U$Year,resid(lm1))
 
 ## ----lagplot_plot,fig.width=3,fig.height=3.5,out.width="2in",echo=F------
 plot(lag_e,e)
-
-## ----construction,echo=T,eval=F------------------------------------------
-## L <- read.table(file="life_expectancy.txt",header=TRUE)
-## L_fit <- lm(Total~Year,data=L)
-## L_detrended <- L_fit$residuals
-## U <- read.table(file="unemployment.csv",sep=",",header=TRUE)
-## U_annual <- apply(U[,2:13],1,mean)
-## U_detrended <- lm(U_annual~U$Year)$residuals
-## L_detrended <- subset(L_detrended,L$Year %in% U$Year)
-
-## ----plot_L_code,eval=F,echo=T-------------------------------------------
-## plot(Total~Year,data=L,type="l") # L is life expectancy
-
-## ----plot_L,echo=F,fig.height=3,fig.width=6,out.width="4.5in"------------
-plot(Total~Year,data=L,type="l") # L is life expectancy
-
-## ----plot_U_code,eval=F,echo=T-------------------------------------------
-## plot(U_annual~Year,data=U,type="l") # U is unemployment
-
-## ----plot_U,echo=F,fig.height=3,fig.width=6,out.width="4.5in"------------
-plot(U_annual~Year,data=U,type="l") # U is unemployment
-
-## ----plot_L_detrended_code,eval=F,echo=T---------------------------------
-## L_fit <- lm(Total~Year,data=L)
-## L_detrended <- L_fit$residuals
-## plot(L_detrended~Year,data=L)
-
-## ----plot_L_detrended,echo=F,fig.height=3,fig.width=5,out.width="3.3in"----
-L_fit <- lm(Total~Year,data=L)
-L_detrended <- L_fit$residuals
-plot(L_detrended~Year,data=L)
-
-## ----plot_L_detrended_subset_code,eval=F,echo=T--------------------------
-## L_detrended <- subset(L_detrended,L$Year %in% U$Year)
-## plot(L_detrended~Year,data=U)
-
-## ----plot_L_detrended_subset,echo=F,fig.height=3,fig.width=5,out.width="3.3in"----
-L_detrended <- subset(L_detrended,L$Year %in% U$Year)
-plot(L_detrended~Year,data=U)
 
